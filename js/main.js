d8e08132d2e3a9b2572be0de0bcb6e810ddac7b5
Erik Opinaldo <erikopinaldo@gmail.com>
2022-05-29 10:37:35 -0400

    added textContent to try block

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ async function makeReq(){
     try {
         const res = await fetch(`/api/result`)
         const data = await res.json()
+        console.log(data);
+        document.querySelector("#result").textContent = data.result
     }
     catch(error) {
         console.log(error)
     }
-    console.log(data);
-    document.querySelector("#result").textContent = data.result
 }
