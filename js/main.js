ced1e576bb97eaad9dde2cf3dc8e30afd8fb4501
Erik Opinaldo <erikopinaldo@gmail.com>
2022-05-29 10:36:08 -0400

    added try catch for API request

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 document.querySelector('#clickMe').addEventListener('click', makeReq)
 
 async function makeReq(){
-
-  const res = await fetch(`/api/result`)
-  const data = await res.json()
-
-  console.log(data);
-  document.querySelector("#result").textContent = data.result
+    try {
+        const res = await fetch(`/api/result`)
+        const data = await res.json()
+    }
+    catch(error) {
+        console.log(error)
+    }
+    console.log(data);
+    document.querySelector("#result").textContent = data.result
 }
